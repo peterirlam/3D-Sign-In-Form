9955b3a1f5d6c03addbcb61c1f1683bbf9982d0d
Peter Irlam <irlampeter@gmail.com>
2021-10-20 10:20:09 +0100

    Update with welcome message
    Welcome message extracts the first name from the full name entered by user and resets when the sign in / sign up buttons are clicked

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
 let signUpBtn = document.querySelector(".signup-btn");
 let signInBtn = document.querySelector(".signin-btn");
 let formsWrapper = document.querySelector(".forms-wrapper");
+let newMsg = document.querySelector("#msg");
 
 signUpBtn.addEventListener("click", (e) => {
   e.preventDefault();
   formsWrapper.classList.add("change");
+  document.querySelector("#msg").innerHTML = "";
 });
 
 signInBtn.addEventListener("click", (e) => {
   e.preventDefault();
   formsWrapper.classList.remove("change");
+  document.querySelector("#msg").innerHTML = "";
+});
+
+  e.preventDefault();
+  newMsg.append(`Good to have you back!`);
+});
+
+signUpSubmit.addEventListener("click", (e) => {
+  e.preventDefault();
+  let newMember = document.querySelector("#name").value.split(" ")[0]; 
+  newMsg.append(`Welcome to the forum ${newMember}!`); 
 });
